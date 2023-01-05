aebeb7c107da6938ca6d3d8c3413b21655d754ae
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2023-01-05 12:04:00 +0000

    first commit

---
+++ src/main/java/example/backcontrolefacile/BackControleFacileApplication.java
@@ -0,0 +1,13 @@
+package example.backcontrolefacile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackControleFacileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackControleFacileApplication.class, args);
+    }
+
+}
